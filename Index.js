2598785d89130c307f8ab146c02ecf6536a74d73
raj <pradeepraj9704@gmail.com>
2023-10-25 10:51:14 +0530

    inital commit

--- Index.js
+++ Index.js
@@ -1,30 +1,29 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-app.use(express.json());
+const bodyParser = require("body-parser");
 const cors = require("cors");
-app.use(cors());
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
+app.use(bodyParser.json());
+app.use(cors());
 
-
-const JWT_SECRET = "sdfghjkl;kjhgfdghjkljhgfhjkljhgfh9876546789876578()././.";
+// Define a secure JWT secret. Use environment variables for this purpose.
+const JWT_SECRET = "your-secret-key-goes-here";
 
 const mongoUrl = "mongodb+srv://praneshkangeyanse21it:sepk2003@cluster0.uyh6rjw.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose.connect(mongoUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-
 })
     .then(() => {
         console.log("Connected to database");
     })
     .catch((e) => console.log(e));
 
-require("./user");
-
-const User = mongoose.model("UserInfo");
+// Define your user model here or import it as needed
 
 app.post("/register", async (req, res) => {
     const { fname, lname, email, pass } = req.body;
@@ -33,61 +32,50 @@ app.post("/register", async (req, res) => {
     try {
         const oldUser = await User.findOne({ email });
         if (oldUser) {
-            return res.send({ error: "User Exists" });
+            return res.status(400).json({ error: "User Exists" }); // Use a proper status code
         }
         await User.create({
             fname,
-            lname, email,
+            lname,
+            email,
             pass: encryptedPassword,
         });
-        res.send({ status: "ok" });
+        res.json({ status: "ok" });
     } catch (error) {
-        res.send({ status: "error" });
+        res.status(500).json({ status: "error" }); // Use a proper status code
     }
-
 });
+
 app.post("/login", async (req, res) => {
-    const { email } = req.body;
+    const { email, pass } = req.body;
     const user = await User.findOne({ email });
     if (!user) {
-        return res.json({ status: "error", error: "User Not Found" }); // Send an error status
+        return res.status(400).json({ status: "error", error: "User Not Found" });
     }
     if (await bcrypt.compare(pass, user.pass)) {
-        const token = jwt.sign({}, JWT_SECRET);
-
-        if (res.status(201)) {
-            return res.json({ status: "ok", data: token });
-        } else {
-            return res.json({ status: "error", error: "Server Error" }); // Send an error status
-        }
+        const token = jwt.sign({ email: user.email }, JWT_SECRET);
+        res.json({ status: "ok", data: token });
+    } else {
+        res.status(400).json({ status: "error", error: "Invalid Password" });
     }
-    res.json({ status: "error", error: "Invalid Password" });
 });
-
 
 app.post("/userData", async (req, res) => {
     const { token } = req.body;
     try {
-      const user = jwt.verify(token, JWT_SECRET);
-      const useremail = user.email;
-      User.findOne({ email: useremail })
-        .then((data) => {
-          if (data) {
-            res.json({ status: "ok", data: data }); // Send the user data as JSON
-          } else {
-            res.json({ status: "error", data: "User not found" });
-          }
-        })
-        .catch((error) => {
-          res.json({ status: "error", data: error });
-        });
+        const user = jwt.verify(token, JWT_SECRET);
+        const useremail = user.email;
+        const data = await User.findOne({ email: useremail });
+        if (data) {
+            res.json({ status: "ok", data });
+        } else {
+            res.status(400).json({ status: "error", data: "User not found" });
+        }
     } catch (error) {
-      res.json({ status: "error", data: error });
+        res.status(400).json({ status: "error", data: error });
     }
-  });
-  
-
+});
 
 app.listen(5000, () => {
-    console.log("server started");
+    console.log("Server started on port 5000");
 });
